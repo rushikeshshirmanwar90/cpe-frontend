22e6785c450dbf59796bfb93955916cf3d1c1c98
= <=>
2024-03-14 16:37:49 +0530

    adding college section

---
+++ app/college/page.tsx
@@ -0,0 +1,12 @@
+import { CollegeHomeSection } from "@/components/custom/hero-section";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <CollegeHomeSection />
+    </div>
+  );
+};
+
+export default page;
